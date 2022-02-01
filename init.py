95b39ed029ab82224fada175850c2437c835b242
Jerome <zjrtomjohn@hotmail.com>
2022-02-01 10:34:55 -0800

    Update init.py

--- init.py
+++ init.py
@@ -33,7 +33,7 @@ def main(now):
 
   for friend in friends:
     name = friend["name"]
-    id = friend["id"]
+    id = venmo.get_user_id_by_username(friend["id"])
     description = "HBO Max for the month of " + month + " — Sent by JrZ's GitHub Actions"
     amount = 14.99 / 2
     message = f"""Good news old sport!
